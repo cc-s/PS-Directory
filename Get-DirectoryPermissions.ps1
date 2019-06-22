88229ee776c41a8cb00e3dac307d425bd2d53096
cc <cc@CCs-MacPro.local>
2019-06-22 12:45:11 +0200

    ps

---
+++ Get-DirectoryPermissions.ps1
@@ -0,0 +1,161 @@
+﻿<#
+.Synopsis
+   Retrieves permissions of directories
+.DESCRIPTION
+   
+.EXAMPLE
+Retrieves only the permissions of the directories in the targeted folder
+   Get-DirectoryPermissions -Path C:\Skripts
+.EXAMPLE
+Retrieves  the permissions of the directories in the targeted folder and subfolders
+   Get-DirectoryPermissions -Path C:\Skripts -Recurce:$true
+.INPUTS
+   
+.OUTPUTS
+
+.NOTES
+  Ti
+.COMPONENT
+   
+.ROLE
+  Permissions
+.FUNCTIONALITY
+   
+#>
+function Get-DirectoryPermissions {
+    [CmdletBinding(DefaultParameterSetName = 'Parameter Set 1', 
+        SupportsShouldProcess = $true, 
+        PositionalBinding = $false,
+        HelpUri = 'ti.novelnetworking.se/',
+        ConfirmImpact = 'Medium')]
+    [Alias()]
+    [OutputType([String])]
+    Param
+    (
+        # Path to folder
+        [Parameter(Mandatory = $true, 
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true, 
+            ValueFromRemainingArguments = $false, 
+            Position = 0,
+            ParameterSetName = 'Parameter Set 1')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [Alias("P")] 
+        $Path,
+
+        [Parameter(Mandatory = $false, 
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true, 
+            ValueFromRemainingArguments = $false, 
+            Position = 1,
+            ParameterSetName = 'Parameter Set 1')]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [Alias("R")]
+        [Switch]
+        $Recurce = $false
+
+    
+    )
+
+    Begin {
+
+        $Directories = Get-ChildItem -Path $Path  -Recurse:$Recurce -Directory 
+
+        $Acls = $Directories | Get-Acl
+
+    }
+    Process {
+
+        $obj = @()
+
+        foreach ($acl in $Acls) {
+            
+            $Path = $acl.PSPath -replace ("Microsoft.PowerShell.Core\\FileSystem::", "")
+
+            $sddl = $acl.Sddl | ConvertFrom-SddlString
+
+            $Permissions = $sddl.DiscretionaryAcl | ConvertFrom-String -Delimiter ":" -PropertyNames User, Permissions
+            $USR = @()
+            $Permissions | ForEach-Object {
+
+            
+                $PropsPer = @{ 
+                    User       = $_.User
+                    Permission = $_.Permissions
+                }
+
+                $USR += New-Object -TypeName PSobject -Property $PropsPer 
+
+            }
+
+
+            $Props = @{ 
+                Path        = $Path
+                Users       = $Permissions.User
+                Permissions = $USR
+            }
+
+            $Obj += New-Object -TypeName PSobject -Property $Props 
+
+
+        }#end foreach
+
+    }
+    End {
+
+        $Obj
+    }
+}#End function DirectoryPermission
+
+
+
+function Get-SubDirPermission {
+[CmdletBinding(DefaultParameterSetName = 'Parameter Set 1', 
+    SupportsShouldProcess = $true, 
+    PositionalBinding = $false,
+    HelpUri = 'ti.novelnetworking.se/',
+    ConfirmImpact = 'Medium')]
+param (
+     # Path to root folder
+     [Parameter(Mandatory = $false, 
+     ValueFromPipeline = $true,
+     ValueFromPipelineByPropertyName = $true, 
+     ValueFromRemainingArguments = $false, 
+     Position = 0,
+     ParameterSetName = 'Parameter Set 1')]
+ [ValidateNotNull()]
+ [ValidateNotNullOrEmpty()]
+ [Alias("RP")] 
+ $RootPath
+
+)
+
+if ($RootPath -ne $true){
+#Select root folder path
+    Add-Type -AssemblyName System.Windows.Forms
+    $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog  -Property @{ RootFolder = "MyComputer"}
+    $null = $FolderBrowser.ShowDialog()
+    $RootPath = "$($FolderBrowser.SelectedPath)"
+} #End if 
+
+ Write-Verbose "The selected Root Folder is $RootPath"
+
+#Select subfolder path
+$Grid = Get-DirectoryPermissions -Path $RootPath | Out-GridView -PassThru
+    Write-Verbose "The selected subfolder is $Grid"
+
+    if ($Grid.count -gt 1) {
+    Write-Error "To many directories selected"
+        break
+
+    }
+#Retrieve subfolder permission
+$Perm = Get-DirectoryPermissions -Path $RootPath | Where-Object { $_.Path -eq "$($Grid.path)"  } 
+
+$Perm.permissions | ForEach-Object {$_ | Select-Object @{name="Path";e={$($Grid.path)}},User,Permission} 
+
+
+
+}#End function SubDirPermission
